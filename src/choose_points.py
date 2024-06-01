36dd614843fd86f3a1d54a25cb63ea0bd8ed449f
hikamizan <hikamizan771@gmail.com>
2024-06-01 17:02:54 +0700

    update choose point function to really make sure that the point corresponds to each other

--- src/choose_points.py
+++ src/choose_points.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from find_features import img1, img2, pts1, pts2
+from math import sqrt
 
 class ChooseFeatures:
     def __init__(self, img1=None, img2=None):
@@ -9,19 +10,45 @@ class ChooseFeatures:
         self.src_pts = []
         self.dst_pts = []
     
-    def select_src_pts(self, event, x, y, flags, params):
+    def select_pts(self, event, x, y, flags, params):
+        global src_x, src_y
         if event == cv2.EVENT_LBUTTONDBLCLK:
-            cv2.circle(self.img1, (x, y), 5, (255, 255, 255), -1)
+            src_x, src_y = x, y
 
-    def select_dst_pts(self):
+            distances = []
+            for x_features, y_features in zip(pts1[0], pts1[1]):
+                x_nearest = x_features - x
+                y_nearest = y_features - y
+                distance = sqrt(x_nearest**2 + y_nearest**2)
+                distances.append(distance)
+    
+            nearest_index = np.argmin(distances)
+            nearest_x_img1 = pts1[0][nearest_index]
+            nearest_y_img1 = pts1[1][nearest_index]
+
+            nearest_x_img2 = pts2[0][nearest_index]
+            nearest_y_img2 = pts2[1][nearest_index]
+
+            cv2.circle(self.img1, (int(nearest_x_img1), int(nearest_y_img1)), 5, (255, 255, 255), -1)
+            cv2.circle(self.img2, (int(nearest_x_img2), int(nearest_y_img2)), 5, (255, 255, 255), -1)
+
+            self.src_pts.append([nearest_x_img1, nearest_y_img1])
+            self.dst_pts.append([nearest_x_img2, nearest_y_img2])
+            print(self.src_pts, self.dst_pts)
+
+
+    def random_select_pts(self, n_pts: int, dist_pts: str):
         pass
 
-cv2.namedWindow('image')
-features = ChooseFeatures(img1=img1)
-cv2.setMouseCallback('image', features.select_src_pts)
+cv2.namedWindow('image_1')
+
+features = ChooseFeatures(img1=img1, img2=img2)
+
+cv2.setMouseCallback('image_1', features.select_pts)
 
 while True:
-    cv2.imshow('image', img1)
+    cv2.imshow('image_1', img1)
+    cv2.imshow('image_2', img2)
     if cv2.waitKey(20) & 0xFF == 27:
         break
 
